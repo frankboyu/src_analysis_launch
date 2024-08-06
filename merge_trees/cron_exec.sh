2f4649558fc73a8b8fd904a337260d0153dde543
Bo Yu <by51@duke.edu>
2024-08-06 11:12:07 -0400

    Correct the directory of the crontab script

--- merge_trees/cron_exec.sh
+++ merge_trees/cron_exec.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-VERSION="ver08_batch01"
+VERSION="ver09_batch01"
 
 date
 source /group/halld/Software/build_scripts/gluex_env_boot_jlab.sh
 gxenv $HALLD_VERSIONS/version.xml
 export PATH=/site/bin:${PATH} #because .login isn't executed, and need this path for SWIF
-python /work/halld2/home/boyu/src_analysis_launch/merge_trees/merge_trees.py /work/halld2/home/boyu/src_analysis_launch/merge_trees/configs/jobs_merge/jobs_merge_2021-11_${VERSION}.cfg 0
+python /work/halld2/home/boyu/src_analysis_launch/merge_trees/merge_trees.py /work/halld2/home/boyu/src_analysis_launch/merge_trees/configs/jobs_merge_2021-11_${VERSION}.cfg 0
