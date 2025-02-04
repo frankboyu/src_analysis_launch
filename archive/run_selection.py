35c9765062ee16745bc78a1fbcc25d4740ac7805
Bo Yu <by51@duke.edu>
2025-02-04 08:12:23 -0500

    Edit script to check the list of production runs and number of REST files

--- archive/run_selection.py
+++ archive/run_selection.py
@@ -2,18 +2,17 @@ import rcdb
 import os
 
 #query = input("Input the selection query (including the quotation marks ""): \n")
- 
-db = rcdb.RCDBProvider("mysql://rcdb@hallddb/rcdb")
+db = rcdb.RCDBProvider(os.environ.get('RCDB_CONNECTION'))
 
-
-good_runs = db.select_runs("@is_src_production and @status_approved", 90001, 90662)
+good_runs = db.select_runs("@is_src_production and @status_approved and run_config == 'FCAL_BCAL_PS_SRC_m9.conf' and beam_on_current > 55.0 and target_type=='FULL & Ready Carbon'", 90001, 90662)
 #good_runs = db.select_runs("@is_primex_production", 110000, 119999)
 
+rest_count = 0
 print("List of selected runs:")
 for run in good_runs:
-    
+
     print(run.number)
-    
+
     #rawdata_count = 0
     #rawdata_path = '/mss/halld/RunPeriod-2021-11/rawdata/Run0' + str(run.number)
     #for path in os.listdir(rawdata_path):
@@ -22,10 +21,10 @@ for run in good_runs:
     #            rawdata_count += 1
     #print(rawdata_count)
 
-    #rest_count = 0
-    #rest_path = '/mss/halld/RunPeriod-2021-11/recon/ver01/REST/0' + str(run.number)
-    #for path in os.listdir(rest_path):
-    #    if os.path.isfile(os.path.join(rest_path, path)):
-    #        if path[0:9] == 'dana_rest':
-    #            rest_count += 1
-    #print(rest_count)
+    rest_path = '/mss/halld/RunPeriod-2021-11/recon/ver02/REST/0' + str(run.number)
+    for path in os.listdir(rest_path):
+        if os.path.isfile(os.path.join(rest_path, path)):
+            if path[0:9] == 'dana_rest':
+                rest_count += 1
+
+print(rest_count)
