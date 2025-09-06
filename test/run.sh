6553024ed72705e05978338e18a36451141df353
Bo Yu <by51@duke.edu>
2025-09-06 14:25:40 -0400

    Test ver12 analysis launch with the new deuterium cook

--- test/run.sh
+++ test/run.sh
@@ -10,37 +10,37 @@ if   [ "${MODE}" == "get" ]
 then
     if   [ "${TARGET}" == "2H"  ]
     then
-        jcache get /mss/halld/RunPeriod-2021-11/recon/ver01/REST/090213/dana_rest_090213_000.hddm
+        jcache get /mss/halld/RunPeriod-2021-11/recon/ver04/REST/090213/dana_rest_090213_000.hddm
     elif [ "${TARGET}" == "4He" ]
     then
         jcache get /mss/halld/RunPeriod-2021-11/recon/ver01/REST/090061/dana_rest_090061_000.hddm
     elif [ "${TARGET}" == "12C" ]
     then
-        jcache get /mss/halld/RunPeriod-2021-11/recon/ver01/REST/090290/dana_rest_090290_000.hddm
+        jcache get /mss/halld/RunPeriod-2021-11/recon/ver02/REST/090290/dana_rest_090290_000.hddm
     fi
 elif [ "${MODE}" == "check" ]
 then
     if   [ "${TARGET}" == "2H"  ]
     then
-        ls -lh /cache/halld/RunPeriod-2021-11/recon/ver01/REST/090213
+        ls -lh /cache/halld/RunPeriod-2021-11/recon/ver04/REST/090213
     elif [ "${TARGET}" == "4He" ]
     then
         ls -lh /cache/halld/RunPeriod-2021-11/recon/ver01/REST/090061
     elif [ "${TARGET}" == "12C" ]
     then
-        ls -lh /cache/halld/RunPeriod-2021-11/recon/ver01/REST/090290
+        ls -lh /cache/halld/RunPeriod-2021-11/recon/ver02/REST/090290
     fi
 elif [ "${MODE}" == "run" ]
 then
     if   [ "${TARGET}" == "2H"  ]
     then
-        hd_root --loadconfigs test.cfg /cache/halld/RunPeriod-2021-11/recon/ver01/REST/090213/dana_rest_090213_000.hddm
+        hd_root --loadconfigs test.cfg /cache/halld/RunPeriod-2021-11/recon/ver04/REST/090213/dana_rest_090213_000.hddm
     elif [ "${TARGET}" == "4He" ]
     then
         hd_root --loadconfigs test.cfg /cache/halld/RunPeriod-2021-11/recon/ver01/REST/090061/dana_rest_090061_000.hddm
     elif [ "${TARGET}" == "12C" ]
     then
-        hd_root --loadconfigs test.cfg /cache/halld/RunPeriod-2021-11/recon/ver01/REST/090290/dana_rest_090290_000.hddm
+        hd_root --loadconfigs test.cfg /cache/halld/RunPeriod-2021-11/recon/ver02/REST/090290/dana_rest_090290_000.hddm
     fi
 elif [ "${MODE}" == "clear" ]
 then
